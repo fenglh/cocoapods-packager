89c7d5660ae30c360f035a71b2b13df5071e3ca9
冯立海 <fenglh@flatincbr.com>
2025-03-07 11:21:39 +0800

    bug fix

--- make.sh
+++ make.sh
@@ -4,4 +4,4 @@ PASSWORD="bml123456"
 echo $PASSWORD | sudo -S true  # 提前输入密码刷新缓存
 sudo gem uninstall cocoapods-pack
 gem build cocoapods-pack.gemspec
-sudo gem install cocoapods-pack-1.5.0.gem --local
+sudo gem install cocoapods-pack-1.0.0.gem --local
